da7173617dc1e5a80ecf8af9bdc10e3b01c8345d
herculanosilva <contato.herculanosilva@gmail.com>
2022-05-06 14:21:36 -0300

    Stage 2

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+//adicionando ou removendo a class scroll 
 function onScroll(){
-    navigation.classList.add('scroll')
+    if(scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else{
+        navigation.classList.remove('scroll')
+    }
+
 }
 
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
