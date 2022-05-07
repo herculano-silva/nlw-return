6e24b876114576a5eca8519a337f00327a028cb1
herculanosilva <contato.herculanosilva@gmail.com>
2022-05-07 11:44:03 -0300

    Stage 3

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    
+    `);
