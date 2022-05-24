fe59288dfd5a9d8e743f7ab3ec6d5dbd44589708
herculanosilva <contato.herculanosilva@gmail.com>
2022-05-24 17:23:39 -0300

    Stege 5

--- main.js
+++ main.js
@@ -5,36 +5,57 @@ onScroll();
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
-  activeMenuAtCurrentSection()
+
+  activeMenuAtCurrentSection(home)
+  activeMenuAtCurrentSection(services)
+  activeMenuAtCurrentSection(about)
+  activeMenuAtCurrentSection(contact)
 }
 
-function activeMenuAtCurrentSection() {
+function activeMenuAtCurrentSection(section) {
  const targetLine = scrollY + innerHeight / 2
  
   //  verificar se seção passou da linha
   // quais dados vou precisar?
 
   // o topo da seção
-  const sectionTop = services.offsetTop
+  const sectionTop = section.offsetTop
 
   // a altura da seção
-  const sectionHeight = home.offsetHeight
+  const sectionHeight = section.offsetHeight
   
   // o topo da seção chegou ou ultrapassou a linha alvo 
   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
 
   // informações dos dados e da logica
-  console.log('o topo da seção chegou ou passou da linha', sectionTopReachOrPassedTargetline)
+  //console.log('o topo da seção chegou ou passou da linha', sectionTopReachOrPassedTargetline)
 
   // verificar se a base está abaixo da linha alvo
   // quais dados vou precisar?
 
   // a seção termina onde?
   const sectionEndAt = sectionTop + sectionHeight
-  console.log(sectionEndAt)
 
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndAt <= targetLine
+  //console.log('o fundo da seção passou da linda?', sectionEndPassedTargetline)  
 
+  // limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  // console.log(sectionBoundaries)
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries){
+    menuElement.classList.add('active')
+ }
 }
+
+
+
 
 function showNavOnScroll(){
   if (scrollY > 0) {
@@ -63,7 +84,7 @@ function closeMenu() {
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 900,
+  duration: 1000,
 }).reveal(`
   #home, 
   #home img, 
@@ -74,3 +95,4 @@ ScrollReveal({
   #about, 
   #about header, 
   #about .content`)
+
